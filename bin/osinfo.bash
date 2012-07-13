0a55fc63507a214db00c7c564bf4598893a1a043
Anthony Cook <anthonymichaelcook@gmail.com>
2012-07-12 18:16:38 -0700

    Output each item on its own line. Shebang, sanity checks.

--- bin/osinfo.bash
+++ bin/osinfo.bash
@@ -1,5 +1,9 @@
-# Detect platform
+#!/usr/bin/env bash
+
+# Gather uname
 UNAME=$(uname)
+
+# Detect platform
 case $UNAME in
   Darwin)        # OS X
     export PLATFORM='osx'
@@ -8,14 +12,16 @@ case $UNAME in
   Linux)         # Linux
     export PLATFORM='linux'
 
-    # Detect distro
+    # Gather release info
     if [[ -n `command -v lsb_release` ]]; then
+      # dump release info and drop version
       RELEASE=`lsb_release -ds 2>/dev/null | sed 's/^\(.*\) .*/\L\1/'`
     else
+      # grab first entry and remove extraneous path and filename affixes
       RELEASE=`echo /etc/*release | head -1 | sed 's:/etc/\(.*\)-release:\1:'`
     fi
 
-    # detect distro
+    # Detect distro
     case $RELEASE in
       ubuntu)
         export DISTRO='ubuntu'
@@ -24,8 +30,18 @@ case $UNAME in
       gentoo)
         export DISTRO='gentoo'
         ;;
+
+      *)
+        echo 'Unknown distro' $RELEASE
+        exit 2
+        ;;
     esac
+    ;;
+
+  *)
+    echo 'Unknown platform' $UNAME
+    exit 1
     ;;
 esac
 
-echo $PLATFORM $DISTRO
+echo $PLATFORM $DISTRO | sed 's/ /\n/'
