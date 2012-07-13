d8366d73c037c4dc76fb66ea0d06ba20b9440f05
Anthony Cook <anthonymichaelcook@gmail.com>
2012-07-12 17:49:31 -0700

    Script to pull OS and Distro information from system

---
+++ bin/osinfo.bash
@@ -0,0 +1,31 @@
+# Detect platform
+UNAME=$(uname)
+case $UNAME in
+  Darwin)        # OS X
+    export PLATFORM='osx'
+  ;;
+
+  Linux)         # Linux
+    export PLATFORM='linux'
+
+    # Detect distro
+    if [[ -n `command -v lsb_release` ]]; then
+      RELEASE=`lsb_release -ds 2>/dev/null`
+    else
+      RELEASE=`echo /etc/*release | head -1 | sed 's:/etc/\(.*\)-release:\1:'`
+    fi
+
+    # detect distro
+    case $RELEASE in
+      ubuntu)
+        export DISTRO='ubuntu'
+        ;;
+
+      gentoo)
+        export DISTRO='gentoo'
+        ;;
+    esac
+    ;;
+esac
+
+echo $PLATFORM $DISTRO
