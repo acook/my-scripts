54991407d9fefbee18c19e2dfa388be378e7ce64
Anthony Cook <anthonymichaelcook@gmail.com>
2012-07-12 18:30:59 -0700

    Reverting previous commit.

--- bin/osinfo.bash
+++ bin/osinfo.bash
@@ -15,10 +15,10 @@ case $UNAME in
     # Gather release info
     if [[ -n `command -v lsb_release` ]]; then
       # dump release info and drop version
-      RELEASE=`lsb_release -ds 2>/dev/null | sed 's/.*/\L/'`
+      RELEASE=`lsb_release -ds 2>/dev/null | sed 's/^\(.*\) .*/\L\1/'`
     else
       # grab first entry and remove extraneous path and filename affixes
-      RELEASE=`echo /etc/*release | head -1 | sed 's/.*/\L/'`
+      RELEASE=`echo /etc/*release | head -1 | sed 's:/etc/\(.*\)-release:\1:'`
     fi
 
     # Detect distro
