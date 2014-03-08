5c1a4b7c5d334ca5ac35d4eb1a0ac11f228c15df
Anthony Cook <anthonymichaelcook@gmail.com>
2014-03-08 16:10:40 -0600

    Moving mercurial setup to user_setup directory.

--- scripts/install_mercurial.bash
+++ scripts/install_mercurial.bash
@@ -3,8 +3,8 @@
 echo setting up mercurial...
 sudo easy_install pip
 sudo pip install Mercurial
-mkdir ~/Pork
-cd ~/Pork
+mkdir ~/.user_setup
+cd ~/.user_setup
 sudo easy_install hg-git
 sudo easy_install Pygments
 hg clone https://bitbucket.org/durin42/hg-git
