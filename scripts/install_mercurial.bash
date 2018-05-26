3f03091a1fefb0167b3c4eedaaafba0b37928cde
Anthony Cook <github@anthonymcook.com>
2018-05-26 14:18:40 -0500

    Removing outdated tools from setup scripts.

--- scripts/install_mercurial.bash
+++ scripts/install_mercurial.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
 
+cd `dirname $0`/external
+
 echo setting up mercurial...
 sudo easy_install pip
 sudo pip install Mercurial
-mkdir ~/.user_setup
-cd ~/.user_setup
-sudo easy_install hg-git
-sudo easy_install Pygments
+sudo pip install hg-git
+sudo pip install Pygments
 hg clone https://bitbucket.org/durin42/hg-git
 hg clone https://bitbucket.org/sjl/hg-prompt
 hg clone https://bitbucket.org/tksoh/hgshelve
--- scripts/setup_osx.bash
+++ scripts/setup_osx.bash
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# install RVM
-\curl -sSL https://get.rvm.io | bash -s stable --ruby
-
 # install Homebrew
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 
@@ -12,22 +9,18 @@ brew install \
   reattach-to-user-namespace --wrap-launchctl --wrap-pbcopy-and-pbpaste \
   mobile-shell ssh-copy-id autossh \
   coreutils findutils --default-names  moreutils colordiff renameutils \
-  ack the_silver_searcher \
+  sift \
   tmux bash zsh \
   ctags macvim --override-system-vim html2text \
   git --with-brewed-curl --with-gettext --with-pcre
 
-# optional installs
-# brew install redis postgres mysql
-
-# Link Cask apps to Alfred search path
-brew cask alfred link
 # Install some GUI apps with Brew Cask
 brew cask install \
   xquartz tinkertool \
   google-chrome firefox \
   alfred caffeine f-lux spectacle \
   iterm2 github sequel-pro \
-  mou sublime-text textmate light-table brackets \
-  sparrow
+  mou
 
+# Link Cask apps to Alfred search path
+brew cask alfred link
